833e25f35bf3c746b64e421cf4c8122a86ded072
Scott Brady <scott.brady@cfwebtools.com>
2014-10-26 19:59:00 -0500

    Initial UI setup done. Assets installed.

---
+++ SuperSlots/Slot.swift
@@ -0,0 +1,16 @@
+//
+//  Slot.swift
+//  SuperSlots
+//
+//  Created by Scott Brady on 10/26/14.
+//  Copyright (c) 2014 Spider Monkey Tech. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Slot {
+	var value = 0
+	var image = UIImage(named: "Ace")
+	var isRed = true
+}
